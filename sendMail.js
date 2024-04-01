a1a6f5ecb83e57b20efd6614db3248761d957041
MilanGohel <milan.gohel66@gmail.com>
2024-04-01 18:47:21 +0530

    Solved Download Resume error

--- sendMail.js
+++ sendMail.js
@@ -33,14 +33,33 @@ const sendEmail = async (req,res) =>{
 }
 
 app.use(express.static(path.resolve(__dirname,'dist')));
+const staticFilesPath = path.join(__dirname,'dist');
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-
+console.log(staticFilesPath)
 app.post('/email/send', sendEmail);
 
+app.get('/download', function(req, res) {
+    const fileName = '/assests/docs/Milan_Gohel.pdf'; // Replace this with the name of your PDF file
+    
+    // Set the appropriate headers for the response
+    res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
+    res.setHeader('Content-Type', 'application/pdf');
+
+    // Send the file for download
+    res.download(path.join(staticFilesPath, fileName), function(err) {
+        if (err) {
+            // Handle errors, if any
+            console.error('File download failed:', err);
+            res.status(err.status).end();
+        } else {
+            console.log('File downloaded successfully');
+        }
+    });
+});
 app.get('*', (req, res) =>
     res.sendFile(path.resolve('dist','index.html'))
 );
